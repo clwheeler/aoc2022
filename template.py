06110b4870b7d6c9a12e0f024186663ed803a5c2
Charles Wheeler <charles@bluecore.com>
2022-12-01 13:21:38 -0500

    init repo

---
+++ template.py
@@ -0,0 +1,51 @@
+import collections
+import time
+import re
+import sys
+from itertools import cycle
+
+day_str = "00"
+test_input = """x"""
+
+
+def load_inputs(input_str=None):
+    inputs = []
+
+    if input_str:
+        inputs = input_str
+    else:
+        with open('./inputs/day{}_input.txt'.format(day_str), 'r') as f:
+            inputs = f.read()
+
+    parsed_test_input = parse_inputs(inputs)
+    return parsed_test_input
+
+
+def parse_inputs(inputs):
+    parsed = inputs.split('\n')
+    return parsed
+
+
+def solve_part1(start):
+    # inputs = load_inputs(test_input)
+    return "TODO"
+
+
+def solve_part2(start):
+    # inputs = load_inputs(test_input)
+    return "TODO"
+
+
+def run():
+
+    start_time = time.time()
+    print "Part 1:"
+    print solve_part1(0)
+    print "Runtime: {} seconds".format(time.time() - start_time)
+
+    start_time = time.time()
+    print "Part 2:"
+    print solve_part2(0)
+    print "Runtime: {} seconds".format(time.time() - start_time)
+
+run()
